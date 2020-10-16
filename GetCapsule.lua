ef38e3e91f8b9c7c85dd035b263a0ea71702f594
Calvin Lennon <55283009+lennonc1atwit@users.noreply.github.com>
2020-10-16 18:37:59 -0400

    Update GetCapsule.lua
    Fixed extraneous line connections and reformatted how points are connected.

--- GetCapsule.lua
+++ GetCapsule.lua
@@ -12,14 +12,15 @@ local capsuleVertPositions = {
 }
 
 capsuleLineIndices = {
-	0,  4,	16,	28,	40,	52,	64,	73,	70,	58,	46,	34,	22,	10,		
-    0,	0,	1,	13,	25,	37,	49,	61,	73,	67,	55,	43,	31,	19,	
-    7,	0,	61,	62,	63,	64,	65,	66,	67,	68,	69,	70,	71,	72,				
-    61,	49,	50,	51,	52,	53,	54,	55,	56,	57,	58,	59,	60,	49,	
-    37,	38,	39,	40,	41,	42,	43,	44,	45,	46,	47,	48,	37,	25,	
-    26,	27,	28,	29,	30,	31,	32,	33,	34,	35,	36,	25,	13,	14,	
-    15,	16,	17,	18,	19,	20,	21,	22,	23,	24,	13,	1,	2,	3,	
-    4,	5,	6,	7,	8,	9,	10,	11,	12, 1
+    {0 , 4}, {4 ,16}, {16,28}, {28,40}, {40,52}, {52,64}, {64,73}, {73,70}, {70,58}, {58,46}, {46,34}, {34,22}, {22,10}, {10, 0}, -- Oval 1
+    {0 , 1}, {1 ,13}, {13,25}, {25,37}, {37,49}, {49,61}, {61,73}, {73,67}, {67,55}, {55,43}, {43,31}, {31,19}, {19, 7}, {7 , 0}, -- Oval 2
+    {61,62}, {62,63}, {63,64}, {64,65}, {65,66}, {66,67}, {67,68}, {68,69}, {69,70}, {70,71}, {71,72}, {72,61}, -- Start Ring 1
+    {49,50}, {50,51}, {51,52}, {52,53}, {53,54}, {54,55}, {55,56}, {56,57}, {57,58}, {58,59}, {59,60}, {60,49}, -- Start Ring 2
+    {37,38}, {38,39}, {39,40}, {40,41}, {41,42}, {42,43}, {43,44}, {44,45}, {45,46}, {46,47}, {47,48}, {48,37}, -- Start Ring 3
+    {25,26}, {26,27}, {27,28}, {28,29}, {29,30}, {30,31}, {31,32}, {32,33}, {33,34}, {34,35}, {35,36}, {36,25}, -- End Ring 1
+    {13,14}, {14,15}, {15,16}, {16,17}, {17,18}, {18,19}, {19,20}, {20,21}, {21,22}, {22,23}, {23,24}, {24,13}, -- End Ring 2
+    {1 , 2}, {2 , 3}, {3 , 4}, {4 , 5}, {5 , 6}, {6 , 7}, {7 , 8}, {8 , 9}, {9 ,10}, {10,11}, {11,12}, {12, 1}  -- End Ring 3
+
 }
 
 -- rot is a table of vector3's we multiply vec by each coloumn in the matrix to rotate the vector
@@ -27,7 +28,6 @@ local function VectorRotate(vec, rot)
     x = vec:Dot(Vector3(rot[1].x, rot[2].x, rot[3].x))
 	y = vec:Dot(Vector3(rot[1].y, rot[2].y, rot[3].y))
     z = vec:Dot(Vector3(rot[1].z, rot[2].z, rot[3].z))
-   
 	return Vector3(x,y,z)
 end
 
@@ -62,26 +62,22 @@ end
 function getCapsule(vStart, vEnd, flRadius)
     local vCapNorm = (vStart - vEnd)
     vCapNorm:Normalize()
-
-    -- matrix setup is correct
+    -- matrix setup
 	matCapsuleRotationSpace = VectorMatrix(Vector3(0, 0, 1))
     matCapsuleSpace = VectorMatrix(vCapNorm)
    
     -- v stores transformed points to then draw
     local v = {}
-    local vLean = (vEnd - vStart)
     for i = 1, #capsuleVertPositions do
 
         local vecCapsuleVert = Vector3(capsuleVertPositions[i][1], capsuleVertPositions[i][2], capsuleVertPositions[i][3])
-
         -- rotate vectors
         vecCapsuleVert = VectorRotate(vecCapsuleVert, matCapsuleRotationSpace)
         vecCapsuleVert = VectorRotate(vecCapsuleVert, matCapsuleSpace)
-        
         -- scale vectors
         vecCapsuleVert = vecCapsuleVert * flRadius
         if capsuleVertPositions[i][3] > 0 then
-            vecCapsuleVert = vecCapsuleVert + vLean 
+            vecCapsuleVert = vecCapsuleVert + (vEnd - vStart)
         end
         
         v[i] = vecCapsuleVert + vStart
