9bd02110101802c181fc5f62997da290683078fb
Calvin Lennon <55283009+lennonc1atwit@users.noreply.github.com>
2020-12-09 20:20:30 -0500

    Update PerWeaponAPI.lua

--- Per Weapon Gui/PerWeaponAPI.lua
+++ Per Weapon Gui/PerWeaponAPI.lua
@@ -1,143 +1,107 @@
-local SUBGROUPBOX_NAMES     = {"Shared", "Zeus", "Pistol", "Heavy Pistol", "Submachine Gun", "Rifle", "Shotgun", "Scout", "Auto Sniper", "Sniper", "Light Machine Gun"}
-local SUBGROUPBOX_VAR_NAMES = {"shared", "zeus", "pistol", "hpistol",      "smg",            "rifle", "shotgun", "scout", "asniper",     "sniper", "lmg"}
-local ELEMENTS_STORE = {}
+WEAPON_GROUPBOX_NAMES    = {"Shared", "Zeus", "Pistol", "Heavy Pistol", "Submachine Gun", "Rifle", "Shotgun", "Scout", "Auto Sniper", "Sniper", "Light Machine Gun"}
+WEAPON_SNIPER_CONVERSION = {["weapon_awp"] = "Sniper", ["weapon_g3sg1"] = "Auto Sniper", ["weapon_scar20"] = "Auto Sniper", ["weapon_ssg08"] = "Scout"}
+WEAPON_GROUPBOX_VARNAMES = {"shared", "zeus", "pistol", "hpistol",      "smg",            "rifle", "shotgun", "scout", "asniper",     "sniper", "lmg"}
+WEAPON_TYPE_TO_NAME = {[0] = "Zeus", [1] = "Pistol", [2]= "Submachine Gun", [3] = "Rifle", [4] = "Shotgun", [6] = "Light Machine Gun"}
+PERWEAPON_ELEMENTS = {}
 
-PerWeapon = {
-    gui = {},
+function CreatePerWeaponCheckbox(ID, parent, varname, name, value, description)
+    if not PERWEAPON_ELEMENTS[ID] then
+        PERWEAPON_ELEMENTS[ID] = {}
+    end
 
-    refresh = function()
-        -- Get an element table
-        for i, element_table in ipairs(ELEMENTS_STORE) do
-        -- Look at each element
-            local parent_state = string.gsub(element_table[2]:GetValue(), "\"", "")
-            for key, value in pairs(element_table[1]) do
-                -- Set disbaled depending on slected weapon
-                if key == parent_state then
-                    value:SetInvisible(false)
-                else 
-                    value:SetInvisible(true)
-                end
+    for i = 1, 11 do
+        local weapon = WEAPON_GROUPBOX_VARNAMES[i]
+
+        local e = gui.Checkbox(parent, weapon.."."..varname, name, value)
+        e:SetDescription(description)
+
+        if not PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] then
+            PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] = {}
+        end
+
+        PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] =  {e, parent}
+    end
+end
+
+function CreatePerWeaponSlider(ID, parent, varname, name, value, min, max, step, description)
+    if not PERWEAPON_ELEMENTS[ID] then
+        PERWEAPON_ELEMENTS[ID] = {}
+    end
+
+    for i = 1, 11 do
+        local weapon = WEAPON_GROUPBOX_VARNAMES[i]
+
+        local e = gui.Slider(parent, weapon.."."..varname, name, value, min, max, step)
+        e:SetDescription(description)
+
+        if not PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] then
+            PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] = {}
+        end
+
+        PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] =  {e, parent}
+    end
+end
+
+function CreatePerWeaponCombobox(ID, parent, varname, name, description, ...) 
+    if not PERWEAPON_ELEMENTS[ID] then
+        PERWEAPON_ELEMENTS[ID] = {}
+    end
+
+    for i = 1, 11 do
+        local weapon = WEAPON_GROUPBOX_VARNAMES[i]
+
+        local e = gui.Combobox(parent, varname, name, ...)
+        e:SetDescription(description)
+
+        if not PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] then
+            PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] = {}
+        end
+
+        PERWEAPON_ELEMENTS[ID][WEAPON_GROUPBOX_NAMES[i]] =  {e, parent}
+    end
+end
+
+function getActiveWeaponIndex(ref)
+    local active_weapon = entities.GetLocalPlayer():GetPropEntity("m_hActiveWeapon")
+    local weapon_name = active_weapon:GetName()
+
+    if ref:GetValue() == "\"Shared\"" then
+        return "Shared"
+    elseif weapon_name == "weapon_revolver" or weapon_name == "weapon_deagle" then -- heavy pistol filter
+        return "Heavy Pistol"
+    elseif active_weapon:GetWeaponType() == 5 then -- snipers
+         return WEAPON_SNIPER_CONVERSION[weapon_name]
+    else
+        return WEAPON_TYPE_TO_NAME[active_weapon:GetWeaponType()]
+    end
+end
+
+function getActiveWeaponVar(ref)
+    local index = getActiveWeaponIndex(ref)
+
+    for i = 1, 11 do 
+        if WEAPON_GROUPBOX_NAMES[i] == index then
+            return WEAPON_GROUPBOX_VARNAMES[i]
+        end
+    end
+end
+
+function RefreshGUI()
+    for ID, group in pairs(PERWEAPON_ELEMENTS) do 
+        for key, element in pairs(group) do
+            if key == string.gsub(element[2]:GetValue(), "\"", "") then
+                element[1]:SetInvisible(false)
+            else 
+                element[1]:SetInvisible(true)
             end
         end
-    end,
+    end
+end
 
-    shutdown = function()
-        for i, element_table in ipairs(ELEMENTS_STORE) do
-            for key, value in pairs(element_table[1]) do
-                value:Remove()
-            end
+function ShutDown()
+    for ID, group in pairs(PERWEAPON_ELEMENTS) do 
+        for key, element in pairs(group) do
+           element[1]:Remove()
         end
     end
-}
-
-PerWeapon.gui.Checkbox = function(parent, varname, name, value)
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-        elements[SUBGROUPBOX_NAMES[i]] = gui.Checkbox(parent, subvarname, name, value)
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
-end
-
-PerWeapon.gui.Slider = function(parent, varname, name, value, min, max, step)
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-
-        -- Check if user gave a step value
-        if step then
-            elements[SUBGROUPBOX_NAMES[i]] = gui.Slider(parent, subvarname, name, value, min, max, step)
-        else
-            elements[SUBGROUPBOX_NAMES[i]] = gui.Slider(parent, subvarname, name, value, min, max)
-        end
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
-end
-
-PerWeapon.gui.Keybox = function(parent, varname, name, key)
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-        elements[SUBGROUPBOX_NAMES[i]] = gui.Keybox(parent, subvarname, name, key)
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
-end
-
--- I like variable argument counts
-PerWeapon.gui.Combobox = function(parent, varname, name, ...)
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-        elements[SUBGROUPBOX_NAMES[i]] = gui.Combobox(parent, varname, name, ...)
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
-end
-
-PerWeapon.gui.Editbox = function(parent, varname, value)
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-        elements[SUBGROUPBOX_NAMES[i]] = gui.Editbox( parent, varname, value )
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
-end
-
-PerWeapon.gui.Text = function(parent, text) 
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-        elements[SUBGROUPBOX_NAMES[i]] = gui.Text(parent, text)
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
-end
-
-PerWeapon.gui.ColorPicker = function(parent, varname, name, r, g, b, a)
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-        elements[SUBGROUPBOX_NAMES[i]] = gui.Text(parent, text)
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
-end
-
--- this may need more functionallity 
-PerWeapon.gui.Multibox = function (parent, name)
-    local elements = {}
-
-    for i = 1, 11 do
-        local subvarname = SUBGROUPBOX_VAR_NAMES[i]:lower():gsub(" ", "").."."..varname
-        elements[SUBGROUPBOX_NAMES[i]] = gui.Multibox(parent, name)
-    end
-
-    table.insert(ELEMENTS_STORE, {elements, parent})
-
-    return elements
 end
